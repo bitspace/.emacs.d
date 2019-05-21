98ae4a8d7d477ff91b818732e519ad2f60c78824
Chris Woods <chris.woods@libertymutual.com>
2019-05-21 15:28:37 -0400

    added helm

--- init.el
+++ init.el
@@ -35,12 +35,15 @@ There are two things you can do about this warning:
 ;; Turn on line numbers everywhere
 (global-linum-mode 1)
 
+;; soft-wrap everywhere
+(global-visual-line-mode 1)
+
 ;; Load Darkula theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'Darkula t)
+(load-theme 'dracula t)
 
 ;; Change default font
-(set-face-attribute 'default nil :font "Hack Nerd Font Mono")
+;; (set-face-attribute 'default nil :font "Hack Nerd Font Regular-12")
 
 ;; C stuff
 ;; TODO `indent-tabs-mode t`? Also hippie-expand did not complete `c-default-style`
@@ -52,26 +55,17 @@ There are two things you can do about this warning:
 ;; paren automatching
 (electric-pair-mode 1)
 
-;; paredit
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of lisp code." t)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit)
-(add-hook 'ielm-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook #'enale-paredit-mode)
+;; paredit - disable for now
+;; (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of lisp code." t)
+;; (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit)
+;; (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+;; (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+;; (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+;; (add-hook 'scheme-mode-hook #'enale-paredit-mode)
 
 ;; yasnippet
 (require 'yasnippet)
 (yas-global-mode 1)
-
-;; auto-complete
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(ac-config-default)
-;; set the trigger key so that it can work together with yasnippet on tab key, if the word exists in yasnippet,
-;; pressing tab will cause yasnippet to activate, otherwise auto-complete will
-(ac-set-trigger-key "TAB")
-(ac-set-trigger-key "<tab>")
 
 ;; erc
 (require 'erc)
@@ -82,3 +76,45 @@ There are two things you can do about this warning:
 ;; tramp
 (require 'tramp)
 (setq tramp-default-method "ssh")
+
+;; helm customizations
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is too close to "C-x C-c" which quits emacs.
+;; Must set "C-c h" globally because we cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind TAB to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z") 'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p t ; open helm buffer inside current window rather than spawning a new window
+	  helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source
+	  helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+	  helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
+	  helm-ff-file-name-history-use-recentf t
+	  helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+	(let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+	  (overlay-put ov 'window (selected-window))
+	  (overlay-put ov 'face
+				   (let ((bg-color (face-background 'default nil)))
+					 `(:background ,bg-color :foreground ,bg-color)))
+	  (setq-local cursor-type nil))))
+
+(add-hook 'helm-minibuffer-set-up-hook
+		  'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
